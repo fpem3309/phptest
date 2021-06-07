80763ef078098875025d263f06b32f79bd172e5f
KMG <alsrhks2154@gmail.com>
2021-06-07 23:27:17 +0900

    day2

---
+++ page/product.php
@@ -0,0 +1,16 @@
+<?php
+include_once('./_common.php');
+
+$g5['title'] = "力前家俺";
+
+include_once('../head.php');
+?>
+
+<div>
+<p>力前家俺 其捞瘤 涝聪促.<p>
+</div>
+
+
+<?php
+include_once('../_tail.php');
+?>
